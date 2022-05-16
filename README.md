03cec7b2ed97b6575cdbae407e6175cc2b861663
deekshiswamy24 <deekshiswamy@gmail.com>
2022-05-16 13:23:24 +0530

    comments are added

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # calculator
  My  Simple Calculator !
+
+
+  Simple calculator is a Graphical user interface project made by using Tkinter. 
+  
+  Tkinter is a standard python interface to GUI tool kit.
+  It provides  us with a variety of commom GUI elements which we can use to build out interface such as buttons and various entry fields.
+  To made this simple calculator-
+  First ,I created a GUI window using tk() method and using
+  Some tkinter elements called widgets-
+  --> Buttons
+  --> Label
+  --> entry
+  --> Text
+  Some geomentry manager classes-
+  --> pack() method
+  --> place() method 
+   created the interface and  mainloop() method  to run the application untill window closes.
--- calculator.py
+++ calculator.py
@@ -1,23 +1,40 @@
+
+#Python program to create a simple GUI project
+#Calculator using Tkinter
+
+#import modules from tkinter
 import tkinter
 from tkinter import *
+
+
+#Create a main window using Tkinter  variable root
 cal =Tk()
+
+#Set the background color for GUI window
 cal.configure(bg="#17161b")
+
+#Set the title to GUI window
 cal.title("simple calculator")
+
+#set the configuration to  GUI window
 cal.geometry("570x600+100+200")
 
+#Create a global variable
 expression =" "
 
+#Function to clear the input field text
 def clear():
     global expression
     expression =" "
     result.config(text=expression)
 
-
+#FUnction to update the expression
 def show(value):
     global expression
     expression+= value
     result.config(text=expression)
 
+#FUnction to calculate the expression  present in text field box
 def calculate():
     global expression
     res =" "
@@ -29,9 +46,15 @@ def calculate():
             expression=" "
     result.config(text=res)
 
+#Create a text entry field box using label function
 result=Label(cal,width=25,height=2,text="",font=("arial",30))
+
+#Pack is used for placing the text box
 result.pack()
 
+
+#Create  Buttons and place at a particular positions 
+#use command to update expression
 Button(cal,text="c",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#3697f5",command=lambda: clear()).place(x=10,y=100)
 Button(cal,text="/",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#2a2d36",command=lambda: show("/")).place(x=150,y=100)
 Button(cal,text="%",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#2a2d36",command=lambda: show("%")).place(x=290,y=100)
@@ -56,7 +79,8 @@ Button(cal,text=".",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#2a2d36",
 Button(cal,text="=",width=5,height=3,font=("arial",30,"bold"),bd=1,fg="#3697f5",command=lambda: calculate()).place(x=430,y=400)
 
 
-
+#start the main loop
+cal.mainloop()
 
 
 
